453c3ee208c38c49515bb64d69f86b97c5e1c3cb
CloudTechManishShetty <manishshetty820@gmail.com>
2025-03-02 00:22:24 +0530

    login api call changed

--- src/main/resources/static/js/signin.js
+++ src/main/resources/static/js/signin.js
@@ -13,7 +13,7 @@ loginButton.addEventListener('click', (event) => {
     const data = { email, password };
     const jsonData = JSON.stringify(data);
 
-    fetch('http://localhost:8080/api/users/login', {
+    fetch('https://turf-booking-system-app.onrender.com/api/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
